89896ed25f67b9fe1cd8fb01e563baab3e887639
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2024-12-18 16:30:15 -0800

    update OAuth url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import { NextPage } from 'next';
 
 const Home: NextPage = () => {
   const OAUTH_URL =
-    'https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=3934079316919758&redirect_uri=https://localhost:3000/api/auth/instagram/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish';
-
+    'https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=3934079316919758&redirect_uri=https://socialmatic-fsbm0ipbx-kevin-muhlbachs-projects.vercel.app/api/auth/instagram/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish';
   return (
     <>
       <h1>Hello</h1>
