bc939405a3ffdb8b7faffacdfb37764a14b73457
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2024-12-20 22:20:49 -0800

    update OAUTH url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ import { NextPage } from 'next';
 
 const Home: NextPage = () => {
   const OAUTH_URL =
-    'https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=3934079316919758&redirect_uri=https://socialmatic-fsbm0ipbx-kevin-muhlbachs-projects.vercel.app/api/auth/instagram/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish';
+    'https://www.instagram.com/oauth/authorize?' +
+    'client_id=3934079316919758' +
+    '&redirect_uri=https://socialmatic-fsbm0ipbx-kevin-muhlbachs-projects.vercel.app/api/auth/instagram/callback' +
+    '&scope=instagram_business_basic,instagram_business_manage_messages,instagram_business_manage_comments' +
+    '&response_type=code';
+
   return (
     <>
       <h1>Hello</h1>
